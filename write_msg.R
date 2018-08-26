83ccdb2e41755f53d17b4698946a48b84f4d3c69
Dani Navarro <d.navarro@unsw.edu.au>
2018-08-26 17:47:27 +1000

    more characters, longer message

--- write_msg.R
+++ write_msg.R
@@ -2,7 +2,12 @@ library(tidyverse)
 
 char_table <- read_csv(here::here("char_map.csv"))
 
-message <- c("Complex Human Data","Summer School") %>% 
+msg <- list(
+  frame1 = c("Complex Human Data","Summer School"),
+  frame2 = c("Melbourne 2018", "December 9-14")
+)
+
+msg_frame <- msg$frame2 %>% 
   str_split("") %>% 
   imap_dfr(function(x,v) {
     x %>% 
@@ -15,7 +20,7 @@ message <- c("Complex Human Data","Summer School") %>%
   }) %>% 
   mutate(yoff = 10 * (max(yoff) - yoff))
 
-msg_map <- left_join(message, char_table) %>%
+msg_map <- left_join(msg_frame, char_table) %>%
   filter(value == 1)
 
 pic <- msg_map %>% 
