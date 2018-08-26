cd58107b4806ef2b83070b98fd5d37240de7270b
Dani Navarro <d.navarro@unsw.edu.au>
2018-08-26 17:05:42 +1000

    lower case added to font & multiline message

--- write_msg.R
+++ write_msg.R
@@ -1,21 +1,28 @@
-
-
+library(tidyverse)
 
 char_table <- read_csv(here::here("char_map.csv"))
 
-message <- "HELLO" %>% 
-  str_split("") %>% first() %>% 
-  as.tibble() %>% 
-  rename(char = value) %>%
-  mutate(xoff = 6 * (1:n()))
+message <- c("Complex Human Data","Summer School") %>% 
+  str_split("") %>% 
+  imap_dfr(function(x,v) {
+    x %>% 
+      as.tibble() %>% 
+      rename(char = value) %>%
+      mutate(
+        xoff = 6 * (1:n()),
+        yoff = v
+      )
+  }) %>% 
+  mutate(yoff = 10 * (max(yoff) - yoff))
 
 msg_map <- left_join(message, char_table) %>%
   filter(value == 1)
 
 pic <- msg_map %>% 
-  ggplot(aes(x=xpos + xoff, y=ypos)) + 
-  geom_point(size = 5) + 
-  coord_equal() + 
-  ylim(0, 10)
+  ggplot(aes(
+    x = xpos + xoff, 
+    y = ypos + yoff)) + 
+  geom_point() + 
+  coord_equal()
 
 plot(pic)
